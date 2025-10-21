5a7a07f2b8e011c8e5644dbc38e052bf4cc406aa
JoãovictorSC <jv140280@gmail.com>
2025-10-20 21:23:44 -0300

    ✨ feat: add index file for report controllers

---
+++ api/src/presentation/reports/controllers/index.ts
@@ -0,0 +1,6 @@
+export * from './get-daily-sales.controller';
+export * from './get-month-comparison.controller';
+export * from './get-monthly-summary.controller';
+export * from './get-payment-status.controller';
+export * from './get-period-summary.controller';
+export * from './get-top-clients.controller';
