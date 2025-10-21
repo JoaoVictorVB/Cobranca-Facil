d2d1768da5d30ac7306b7d566159f18886ef2969
JoãovictorSC <jv140280@gmail.com>
2025-10-20 21:23:38 -0300

    ✨ feat: refactor ProductModule to remove ProductController and streamline provider setup

--- api/src/presentation/product/product.module.ts
+++ api/src/presentation/product/product.module.ts
@@ -6,51 +6,30 @@ import { FindProductByIdUseCase } from '../../application/product/use-cases/find
 import { UpdateProductUseCase } from '../../application/product/use-cases/update-product.use-case';
 import { PrismaService } from '../../infrastructure/database/prisma.service';
 import { ProductRepository } from '../../infrastructure/repositories/product.repository';
-import { ProductController } from './product.controller';
+import {
+  CreateProductController,
+  DeleteProductController,
+  GetAllProductsController,
+  GetProductByIdController,
+  UpdateProductController,
+} from './controllers';
 
 @Module({
-  controllers: [ProductController],
+  controllers: [
+    CreateProductController,
+    GetAllProductsController,
+    GetProductByIdController,
+    UpdateProductController,
+    DeleteProductController,
+  ],
   providers: [
     PrismaService,
-    {
-      provide: 'IProductRepository',
-      useClass: ProductRepository,
-    },
-    {
-      provide: CreateProductUseCase,
-      useFactory: (productRepository: ProductRepository) => {
-        return new CreateProductUseCase(productRepository);
-      },
-      inject: ['IProductRepository'],
-    },
-    {
-      provide: FindAllProductsUseCase,
-      useFactory: (productRepository: ProductRepository) => {
-        return new FindAllProductsUseCase(productRepository);
-      },
-      inject: ['IProductRepository'],
-    },
-    {
-      provide: FindProductByIdUseCase,
-      useFactory: (productRepository: ProductRepository) => {
-        return new FindProductByIdUseCase(productRepository);
-      },
-      inject: ['IProductRepository'],
-    },
-    {
-      provide: UpdateProductUseCase,
-      useFactory: (productRepository: ProductRepository) => {
-        return new UpdateProductUseCase(productRepository);
-      },
-      inject: ['IProductRepository'],
-    },
-    {
-      provide: DeleteProductUseCase,
-      useFactory: (productRepository: ProductRepository) => {
-        return new DeleteProductUseCase(productRepository);
-      },
-      inject: ['IProductRepository'],
-    },
+    ProductRepository,
+    CreateProductUseCase,
+    FindAllProductsUseCase,
+    FindProductByIdUseCase,
+    UpdateProductUseCase,
+    DeleteProductUseCase,
   ],
 })
 export class ProductModule {}
